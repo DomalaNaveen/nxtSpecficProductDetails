a9f3a1d3f8870b3180a4ed074d3d44f4c89a10a7
DomalaNaveen <naveen9603737739@gmail.com>
2022-12-09 05:56:33 +0000

    nxtspecificProductDetails

---
+++ src/components/SimilarProductItem/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+
+const SimilarProductItem = props => {
+  const {productDetails} = props
+  const {title, brand, imageUrl, rating, price} = productDetails
+
+  return (
+    <li className="similar-product-item">
+      <img
+        src={imageUrl}
+        alt={`similar product ${title}`}
+        className="similar-product-image"
+      />
+      <p className="title">{title}</p>
+      <p className="brand">{brand}</p>
+      <div className="price-rating-container">
+        <p className="price">{price}</p>
+        <div className="rating-container">
+          <p className="rating">{rating}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+            alt="star"
+            className="star-image"
+          />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default SimilarProductItem
